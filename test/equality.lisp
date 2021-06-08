77a26c0ddef338b66d21490f309aa5287d4346ce
Alexander Gutev <alex.gutev@gmail.com>
2021-06-08 09:03:10 +0200

    Replace IS with IS-TRUE in equality tests.

--- test/equality.lisp
+++ test/equality.lisp
@@ -29,25 +29,25 @@
 (test-optimize number-=
   "Test `=` on numbers"
 
-  (is (= 1 1))
-  (is (= 2.0 2))
-  (is (= 6/3 2))
+  (is-true (= 1 1))
+  (is-true (= 2.0 2))
+  (is-true (= 6/3 2))
 
-  (is (not (= 1 0)))
-  (is (not (= 2.3 5.7)))
+  (is-false (= 1 0))
+  (is-false (= 2.3 5.7))
 
-  (is (= 1))
-  (is (= 1 1.0 2/2))
-  (is (not (= 1 1 3))))
+  (is-true (= 1))
+  (is-true (= 1 1.0 2/2))
+  (is-false (= 1 1 3)))
 
 (test-optimize number-/=
   "Test `/=` on numbers"
 
-  (is (/= 1 0))
-  (is (/= 2 3))
-  (is (/= 1))
-  (is (/= 1 1 3))
-  (is (not (/= 1 1.0 2/2))))
+  (is-true (/= 1 0))
+  (is-true (/= 2 3))
+  (is-true (/= 1))
+  (is-true (/= 1 1 3))
+  (is-false (/= 1 1.0 2/2)))
 
 (test-optimize random-number-=
   "Test `=` on random numbers"
@@ -77,17 +77,17 @@
 (test-optimize character-=
   "Test `=` on characters"
 
-  (is (= #\a #\a))
-  (is (= #\0 #\0))
-  (is (= #\b (char (make-string 5 :initial-element #\b) 3)))
-  (is (= #\y (aref (make-array 3 :element-type 'character :initial-contents '(#\x #\y #\z)) 1)))
+  (is-true (= #\a #\a))
+  (is-true (= #\0 #\0))
+  (is-true (= #\b (char (make-string 5 :initial-element #\b) 3)))
+  (is-true (= #\y (aref (make-array 3 :element-type 'character :initial-contents '(#\x #\y #\z)) 1)))
 
-  (is (not (= #\a #\A)))
-  (is (not (= #\x (char (make-string 5 :initial-element #\b) 3))))
-  (is (not (= #\z (aref (make-array 3 :element-type 'character :initial-contents '(#\x #\y #\z)) 1))))
+  (is-false (= #\a #\A))
+  (is-false (= #\x (char (make-string 5 :initial-element #\b) 3)))
+  (is-false (= #\z (aref (make-array 3 :element-type 'character :initial-contents '(#\x #\y #\z)) 1)))
 
-  (is (= #\a #\a #\a))
-  (is (not (= #\a #\A 'a))))
+  (is-true (= #\a #\a #\a))
+  (is-false (= #\a #\A 'a)))
 
 (test-optimize random-character-=
   "Test `=` on random characters"
@@ -103,15 +103,15 @@
 (test-optimize character-/=
   "Test `/=` on characters"
 
-  (is (/= #\a #\A))
-  (is (/= #\x #\y))
-  (is (/= #\x (char (make-string 5 :initial-element #\b) 3)))
-  (is (/= #\z (aref (make-array 3 :element-type 'character :initial-contents '(#\x #\y #\z)) 1)))
-  (is (/= #\d #\d #\e))
+  (is-true (/= #\a #\A))
+  (is-true (/= #\x #\y))
+  (is-true (/= #\x (char (make-string 5 :initial-element #\b) 3)))
+  (is-true (/= #\z (aref (make-array 3 :element-type 'character :initial-contents '(#\x #\y #\z)) 1)))
+  (is-true (/= #\d #\d #\e))
 
-  (is (not (/= #\a #\a #\a)))
-  (is (not (/= #\b (char (make-string 5 :initial-element #\b) 3))))
-  (is (not (/= #\y (aref (make-array 3 :element-type 'character :initial-contents '(#\x #\y #\z)) 1)))))
+  (is-false (/= #\a #\a #\a))
+  (is-false (/= #\b (char (make-string 5 :initial-element #\b) 3)))
+  (is-false (/= #\y (aref (make-array 3 :element-type 'character :initial-contents '(#\x #\y #\z)) 1))))
 
 (test-optimize random-character-/=
   "Test `/=` on random characters"
@@ -130,31 +130,31 @@
 (test-optimize cons-=
   "Test `=` on lists and cons"
 
-  (is (= '(1 2 3) (list 1.0 2 3.0)))
-  (is (= '(a b c) (cons 'a (cons 'b (cons 'c nil)))))
-  (is (= '(1 a #\x) (list 2/2 'a #\x)))
-  (is (= '(((1 2) x y) #\z) (list (list (list 1 2) 'x 'y) #\z)))
-  (is (= '("abc" "def") (list "abc" "def")))
-  (is (= nil (cdr (list '1))))
-  (is (= '(5 6 . 3) '(5.0 6 . 3.0)))
-  (is (= (make-list 100) (make-list 100)))
+  (is-true (= '(1 2 3) (list 1.0 2 3.0)))
+  (is-true (= '(a b c) (cons 'a (cons 'b (cons 'c nil)))))
+  (is-true (= '(1 a #\x) (list 2/2 'a #\x)))
+  (is-true (= '(((1 2) x y) #\z) (list (list (list 1 2) 'x 'y) #\z)))
+  (is-true (= '("abc" "def") (list "abc" "def")))
+  (is-true (= nil (cdr (list '1))))
+  (is-true (= '(5 6 . 3) '(5.0 6 . 3.0)))
+  (is-true (= (make-list 100) (make-list 100)))
 
-  (is (not (= '(1 2 3) '(1 2 1))))
-  (is (not (= '(1 2 3) '(1 2)))))
+  (is-false (= '(1 2 3) '(1 2 1)))
+  (is-false (= '(1 2 3) '(1 2))))
 
 (test-optimize cons-/=
   "Test `/=` on lists and cons"
 
-  (is (not (/= '(1 2 3) (list 1.0 2 3.0))))
-  (is (not (/= '(a b c) (cons 'a (cons 'b (cons 'c nil))))))
-  (is (not (/= '(1 a #\x) (list 2/2 'a #\x))))
-  (is (not (/= '(((1 2) x y) #\z) (list (list (list 1 2) 'x 'y) #\z))))
-  (is (not (/= '("abc" "def") (list "abc" "def"))))
-  (is (not (/= nil (cdr (list '1)))))
-  (is (not (/= '(5 6 . 3) '(5.0 6 . 3.0))))
+  (is-false (/= '(1 2 3) (list 1.0 2 3.0)))
+  (is-false (/= '(a b c) (cons 'a (cons 'b (cons 'c nil)))))
+  (is-false (/= '(1 a #\x) (list 2/2 'a #\x)))
+  (is-false (/= '(((1 2) x y) #\z) (list (list (list 1 2) 'x 'y) #\z)))
+  (is-false (/= '("abc" "def") (list "abc" "def")))
+  (is-false (/= nil (cdr (list '1))))
+  (is-false (/= '(5 6 . 3) '(5.0 6 . 3.0)))
 
-  (is (/= '(1 2 3) '(1 2 1)))
-  (is (/= '(1 2 3) '(1 2))))
+  (is-true (/= '(1 2 3) '(1 2 1)))
+  (is-true (/= '(1 2 3) '(1 2))))
 
 
 ;;; Single-dimensional Arrays (Vector)
@@ -162,42 +162,42 @@
 (test-optimize vector-=
   "Test `=` on single dimensional arrays (vectors)"
 
-  (is (= #(1 2 3) (vector 1 2 3)))
-  (is (= #(1 2 3) (make-array 3
+  (is-true (= #(1 2 3) (vector 1 2 3)))
+  (is-true (= #(1 2 3) (make-array 3
                               :element-type 'number
                               :adjustable t
                               :fill-pointer t
                               :initial-contents '(1 2 3))))
 
-  (is (= #(1 2 x) (vector 1.0 2 'x)))
-  (is (= #(#(1 2) 3) (vector (vector 1.0 2.0) 3)))
-  (is (= #((1 2) 3) (vector '(1.0 2.0) 3)))
+  (is-true (= #(1 2 x) (vector 1.0 2 'x)))
+  (is-true (= #(#(1 2) 3) (vector (vector 1.0 2.0) 3)))
+  (is-true (= #((1 2) 3) (vector '(1.0 2.0) 3)))
 
-  (is (not (= #(1 2 3) #(1 1 1))))
-  (is (not (= #(1 2 3) #(1 2 3 4))))
-  (is (not (= #(1 2 3) (make-array 0))))
-  (is (not (= #(1 2 3) (make-array '(2 2) :initial-contents '((1 2) (3 4))))))
-  (is (not (= #(#(1 2)) #(#(2 1))))))
+  (is-false (= #(1 2 3) #(1 1 1)))
+  (is-false (= #(1 2 3) #(1 2 3 4)))
+  (is-false (= #(1 2 3) (make-array 0)))
+  (is-false (= #(1 2 3) (make-array '(2 2) :initial-contents '((1 2) (3 4)))))
+  (is-false (= #(#(1 2)) #(#(2 1)))))
 
 (test-optimize vector-/=
   "Test `/=` on single dimensional arrays (vectors)"
 
-  (is (not (/= #(1 2 3) (vector 1 2 3))))
-  (is (not (/= #(1 2 3) (make-array 3
+  (is-false (/= #(1 2 3) (vector 1 2 3)))
+  (is-false (/= #(1 2 3) (make-array 3
                                 :element-type 'number
                                 :adjustable t
                                 :fill-pointer t
-                                :initial-contents '(1 2 3)))))
+                                :initial-contents '(1 2 3))))
 
-  (is (not (/= #(1 2 x) (vector 1.0 2 'x))))
-  (is (not (/= #(#(1 2) 3) (vector (vector 1.0 2.0) 3))))
-  (is (not (/= #((1 2) 3) (vector '(1.0 2.0) 3))))
+  (is-false (/= #(1 2 x) (vector 1.0 2 'x)))
+  (is-false (/= #(#(1 2) 3) (vector (vector 1.0 2.0) 3)))
+  (is-false (/= #((1 2) 3) (vector '(1.0 2.0) 3)))
 
-  (is (/= #(1 2 3) #(1 1 1)))
-  (is (/= #(1 2 3) #(1 2 3 4)))
-  (is (/= #(1 2 3) (make-array 0)))
-  (is (/= #(1 2 3) (make-array '(2 2) :initial-contents '((1 2) (3 4)))))
-  (is (/= #(#(1 2)) #(#(2 1)))))
+  (is-true (/= #(1 2 3) #(1 1 1)))
+  (is-true (/= #(1 2 3) #(1 2 3 4)))
+  (is-true (/= #(1 2 3) (make-array 0)))
+  (is-true (/= #(1 2 3) (make-array '(2 2) :initial-contents '((1 2) (3 4)))))
+  (is-true (/= #(#(1 2)) #(#(2 1)))))
 
 
 ;;; Multi-dimensional Arrays
@@ -205,20 +205,20 @@
 (test-optimize nd-array-=
   "Test `=` on multi-dimensional arrays"
 
-  (is (= #2A((1 2 3) (4 5 6)) (make-array '(2 3) :initial-contents '((1 2 3) (4 5 6)))))
-  (is (= #2A((1 (3 4)) (5 #\c)) (make-array '(2 2) :initial-contents '((1 (3 4)) (5 #\c)))))
+  (is-true (= #2A((1 2 3) (4 5 6)) (make-array '(2 3) :initial-contents '((1 2 3) (4 5 6)))))
+  (is-true (= #2A((1 (3 4)) (5 #\c)) (make-array '(2 2) :initial-contents '((1 (3 4)) (5 #\c)))))
 
-  (is (not (= #2A((1 2) (3 4)) #2A((1 1) (3 4)))))
-  (is (not (= #2A((1 2) (3 4)) #(1 2 3 4)))))
+  (is-false (= #2A((1 2) (3 4)) #2A((1 1) (3 4))))
+  (is-false (= #2A((1 2) (3 4)) #(1 2 3 4))))
 
 (test-optimize nd-array-/=
   "Test `/=` on multi-dimensional arrays"
 
-  (is (not (/= #2A((1 2 3) (4 5 6)) (make-array '(2 3) :initial-contents '((1 2 3) (4 5 6))))))
-  (is (not (/= #2A((1 (3 4)) (5 #\c)) (make-array '(2 2) :initial-contents '((1 (3 4)) (5 #\c))))))
+  (is-false (/= #2A((1 2 3) (4 5 6)) (make-array '(2 3) :initial-contents '((1 2 3) (4 5 6)))))
+  (is-false (/= #2A((1 (3 4)) (5 #\c)) (make-array '(2 2) :initial-contents '((1 (3 4)) (5 #\c)))))
 
-  (is (/= #2A((1 2) (3 4)) #2A((1 1) (3 4))))
-  (is (/= #2A((1 2) (3 4)) #(1 2 3 4))))
+  (is-true (/= #2A((1 2) (3 4)) #2A((1 1) (3 4))))
+  (is-true (/= #2A((1 2) (3 4)) #(1 2 3 4))))
 
 
 ;;; Strings
@@ -226,16 +226,16 @@
 (test-optimize string-=
   "Test `=` on strings"
 
-  (is (= "Hello" "Hello"))
-  (is (= "World" (string '|World|)))
-  (is (= "AAA" (make-string 3 :initial-element #\A)))
-  (is (= "abc" (make-array 3 :element-type 'character :initial-contents '(#\a #\b #\c))))
-  (is (= "hello" (vector #\h #\e #\l #\l #\o)))
+  (is-true (= "Hello" "Hello"))
+  (is-true (= "World" (string '|World|)))
+  (is-true (= "AAA" (make-string 3 :initial-element #\A)))
+  (is-true (= "abc" (make-array 3 :element-type 'character :initial-contents '(#\a #\b #\c))))
+  (is-true (= "hello" (vector #\h #\e #\l #\l #\o)))
 
-  (is (not (= "hello" "Hello")))
-  (is (not (= "world" "worlds")))
-  (is (not (= "aaa" (make-string 3 :initial-element #\A))))
-  (is (not (= "cba" (make-array 3 :element-type 'character :initial-contents '(#\a #\b #\c))))))
+  (is-false (= "hello" "Hello"))
+  (is-false (= "world" "worlds"))
+  (is-false (= "aaa" (make-string 3 :initial-element #\A)))
+  (is-false (= "cba" (make-array 3 :element-type 'character :initial-contents '(#\a #\b #\c)))))
 
 (test-optimize random-string-=
   "Test `=` on random strings"
@@ -250,17 +250,17 @@
 (test-optimize string-/=
   "Test `/=` on strings"
 
-  (is (not (/= "Hello" "Hello")))
-  (is (not (/= "World" (string '|World|))))
-  (is (not (/= "AAA" (make-string 3 :initial-element #\A))))
-  (is (not (/= "hello" (vector #\h #\e #\l #\l #\o))))
-  (is (not (/= "abc" (make-array 3 :element-type 'character :initial-contents '(#\a #\b #\c)))))
-  (is (not (/= "hello" (vector #\h #\e #\l #\l #\o))))
+  (is-false (/= "Hello" "Hello"))
+  (is-false (/= "World" (string '|World|)))
+  (is-false (/= "AAA" (make-string 3 :initial-element #\A)))
+  (is-false (/= "hello" (vector #\h #\e #\l #\l #\o)))
+  (is-false (/= "abc" (make-array 3 :element-type 'character :initial-contents '(#\a #\b #\c))))
+  (is-false (/= "hello" (vector #\h #\e #\l #\l #\o)))
 
-  (is (/= "hello" "Hello"))
-  (is (/= "world" "worlds"))
-  (is (/= "aaa" (make-string 3 :initial-element #\A)))
-  (is (/= "cba" (make-array 3 :element-type 'character :initial-contents '(#\a #\b #\c)))))
+  (is-true (/= "hello" "Hello"))
+  (is-true (/= "world" "worlds"))
+  (is-true (/= "aaa" (make-string 3 :initial-element #\A)))
+  (is-true (/= "cba" (make-array 3 :element-type 'character :initial-contents '(#\a #\b #\c)))))
 
 (test-optimize random-string-/=
   "Test `/=` on random strings"
@@ -283,28 +283,28 @@
     (setf (gethash "hello" table) "world")
     (setf (gethash '(1 2 3) table) #\z)
 
-    (is (= table
+    (is-true (= table
            (alist-hash-table
             '((x . 1) (y . z) ("hello" . "world") ((1 2 3) . #\z)))))
 
-    (is (= table
+    (is-true (= table
            (alist-hash-table
             '((x . 1) (y . z) ("HELLO" . "world") ((1 2 3) . #\z)) :test #'equalp)))
 
-    (is (not
+    (is-false
          (= table
             (alist-hash-table
-             '((x . 2) (y . z) ("hello" . "world") ((1 2 3) . #\z))))))
+             '((x . 2) (y . z) ("hello" . "world") ((1 2 3) . #\z)))))
 
-    (is (not
+    (is-false
          (= table
             (alist-hash-table
-             '((x . 2) (y . z) ("hello" . "world") ((1 2 3) . #\z))))))
+             '((x . 2) (y . z) ("hello" . "world") ((1 2 3) . #\z)))))
 
-    (is (not
+    (is-false
          (= table
             (alist-hash-table
-             '((x . 2) (y . z) ("hello" . "world") ((1 2 3) . #\z) ("x" . "z"))))))))
+             '((x . 2) (y . z) ("hello" . "world") ((1 2 3) . #\z) ("x" . "z")))))))
 
 
 ;;; Symbols
@@ -312,30 +312,30 @@
 (test-optimize symbol-=
   "Test `=` on symbols"
 
-  (is (= 'a 'a))
-  (is (= 'sym 'sym))
-  (is (= '|a symbol| '|a symbol|))
-  (is (= :key :key))
+  (is-true (= 'a 'a))
+  (is-true (= 'sym 'sym))
+  (is-true (= '|a symbol| '|a symbol|))
+  (is-true (= :key :key))
 
-  (is (not (= 'a 'b)))
-  (is (not (= 'sym 'syms)))
-  (is (not (= :key1 :key2)))
-  (is (not (= 'a :a)))
-  (is (not (= 'a '#:a))))
+  (is-false (= 'a 'b))
+  (is-false (= 'sym 'syms))
+  (is-false (= :key1 :key2))
+  (is-false (= 'a :a))
+  (is-false (= 'a '#:a)))
 
 (test-optimize symbol-/=
   "Test `/=` on symbols"
 
-  (is (not (/= 'a 'a)))
-  (is (not (/= 'sym 'sym)))
-  (is (not (/= '|a symbol| '|a symbol|)))
-  (is (not (/= :key :key)))
+  (is-false (/= 'a 'a))
+  (is-false (/= 'sym 'sym))
+  (is-false (/= '|a symbol| '|a symbol|))
+  (is-false (/= :key :key))
 
-  (is (/= 'a 'b))
-  (is (/= 'sym 'syms))
-  (is (/= :key1 :key2))
-  (is (/= 'a :a))
-  (is (/= 'a '#:a)))
+  (is-true (/= 'a 'b))
+  (is-true (/= 'sym 'syms))
+  (is-true (/= :key1 :key2))
+  (is-true (/= 'a :a))
+  (is-true (/= 'a '#:a)))
 
 
 ;;; Different Types
